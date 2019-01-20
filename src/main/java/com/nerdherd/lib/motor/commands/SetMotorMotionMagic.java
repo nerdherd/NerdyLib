813453601cfa0c885a3326e3dc77e4e4676dd305
Duncan Wedig <duncan.wedig@gmail.com>
2019-01-19 21:19:29 -0800

    Motion magic command yay

---
+++ src/main/java/com/nerdherd/lib/motor/commands/SetMotorMotionMagic.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.nerdherd.lib.motor.commands;
+
+import com.nerdherd.lib.motor.single.AbstractSingleMotorTalonSRX;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SetMotorMotionMagic extends Command {
+
+  private AbstractSingleMotorTalonSRX m_motor;
+  private double m_pos;
+
+  public SetMotorMotionMagic(AbstractSingleMotorTalonSRX motor, double pos) {
+    m_motor = motor;
+    m_pos = pos;
+    requires(m_motor);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    m_motor.setPositionMotionMagic(m_pos);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
