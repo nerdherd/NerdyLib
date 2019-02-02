38e26c0fec800c42a4e7f96deeca09f64c8e9e9a
Duncan Wedig <duncan.wedig@gmail.com>
2019-02-02 09:44:44 -0800

    We luv height conversions for elevators

--- src/main/java/com/nerdherd/lib/motor/single/mechanisms/SingleMotorElevator.java
+++ src/main/java/com/nerdherd/lib/motor/single/mechanisms/SingleMotorElevator.java
@@ -10,15 +10,35 @@ package com.nerdherd.lib.motor.single.mechanisms;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 
+import badlog.lib.BadLog;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * Add your docs here.
  */
 public class SingleMotorElevator extends GravityAffectedMechanism {
 
+  private double m_distanceRatio, m_distanceOffset;
+
   public SingleMotorElevator(int talonID, String subsystemName, boolean inversion, boolean sensorPhase) {
     super(talonID, subsystemName, inversion, sensorPhase);
     super.m_gravityFF = 0;
     super.m_staticFF = 0;
+    m_distanceRatio = 1;
+    m_distanceOffset = 0;
+  }
+
+  public void configDistanceRatio(double newDistanceRatio) {
+    m_distanceRatio = newDistanceRatio;
+  }
+
+  public void configDistanceOffset(double newDistanceOffset) {
+    m_distanceOffset = newDistanceOffset;
+  }
+
+  public void configAngleConversion(double newDistanceRatio, double newDistanceOffset) {
+    m_distanceRatio = newDistanceRatio;
+    m_distanceOffset = newDistanceOffset;
   }
 
   public double getFFIfMoving() {
@@ -76,6 +96,30 @@ public class SingleMotorElevator extends GravityAffectedMechanism {
     } else {
       m_motor.set(ControlMode.Velocity, vel, DemandType.ArbitraryFeedForward, getFFIfMoving());
     }
+  }
+
+  public double heightToTicks(double height) {
+    return (height - m_distanceOffset) / m_distanceRatio;
+  }
+
+  public double ticksToHeight(double ticks) {
+    return (m_distanceRatio * ticks) + m_distanceOffset;
+  }
+
+  public double getHeight() {
+    return ticksToHeight(this.getPosition());
+  }
+
+  @Override
+  public void reportToSmartDashboard() {
+    super.reportToSmartDashboard();
+    SmartDashboard.putNumber(m_name + " Height", getHeight());
+  }
+
+  @Override
+  public void initLoggingData() {
+    super.initLoggingData();
+    BadLog.createTopic(m_name + "/Height", "in", () -> getHeight());
   }
 
 }
