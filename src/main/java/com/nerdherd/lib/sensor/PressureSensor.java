9c8cafb665fae0bbbb9a294a429608aacd3e1874
lydia <l.neguse03@gmail.com>
2019-03-15 15:17:06 -0700

    Moar pressure sensor stuff yay

--- src/main/java/com/nerdherd/lib/sensor/PressureSensor.java
+++ src/main/java/com/nerdherd/lib/sensor/PressureSensor.java
@@ -20,11 +20,18 @@ public class PressureSensor implements Loggable {
   
     private final AnalogInput m_analogSensor;
     private final String m_name;
+    private double m_pressureOffset;
   
+    public PressureSensor(String name, int port, double pressureOffset) {
+      this(name, port);
+      m_pressureOffset = pressureOffset;
+    }
+
     public PressureSensor(String name, int port) {
         m_name = name;
   
         m_analogSensor = new AnalogInput(port);
+        m_pressureOffset = 0;
     }
     
     public double getVoltage() {
@@ -32,14 +39,17 @@ public class PressureSensor implements Loggable {
     }
     
     public double getPressure() {
-      return 3.51336 * (this.getVoltage() * this.getVoltage())+ 30.3481 * (this.getVoltage()) + 2.62798;
+      return 41.83 * this.getVoltage() - 5.083 + m_pressureOffset;
+      // return 3.51336 * (this.getVoltage() * this.getVoltage())+ 30.3481 * (this.getVoltage()) + 2.62798;
     }
 
     public void reportToSmartDashboard() {
       SmartDashboard.putNumber(m_name + " pressure", getPressure());
+      SmartDashboard.putNumber(m_name + " voltage", getVoltage());
     }
   
     public void initLoggingData() {
       NerdyBadlog.createTopic(m_name + "/Pressure", () -> getPressure());
+      NerdyBadlog.createTopic(m_name + "/Voltage", () -> getVoltage());
     }
 }
