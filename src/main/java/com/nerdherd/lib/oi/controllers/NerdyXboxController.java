7e52ae86744a046611ada02c3de1b5b31f3ae26b
Dylan Barva <d.barva5402@gmail.com>
2019-08-13 20:03:16 -0700

    switch dpad to use POV

--- src/main/java/com/nerdherd/lib/oi/controllers/NerdyXboxController.java
+++ src/main/java/com/nerdherd/lib/oi/controllers/NerdyXboxController.java
@@ -9,6 +9,7 @@ package com.nerdherd.lib.oi.controllers;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.POVButton;
 
 /**
  * Add your docs here.
@@ -19,22 +20,21 @@ public class NerdyXboxController extends XboxController{
      * 
      * ALL BUTTON NUMBERS ARE PLACE HOLDERS
      *  */
-    public JoystickButton startButton = new JoystickButton(this, 0);
-    public JoystickButton selectButton = new JoystickButton(this, 0);
-    public JoystickButton aButton = new JoystickButton(this, 0);
-    public JoystickButton bButton = new JoystickButton(this, 0);
-    public JoystickButton yButton = new JoystickButton(this, 0);
-    public JoystickButton xButton = new JoystickButton(this, 0);
-    public JoystickButton upDPad = new JoystickButton(this, 0);
-    public JoystickButton downDPad = new JoystickButton(this, 0);
-    public JoystickButton leftDPad = new JoystickButton(this, 0);
-    public JoystickButton rightDPad = new JoystickButton(this, 0);
-    public JoystickButton leftBumper = new JoystickButton(this, 0);
-    public JoystickButton rightBumper = new JoystickButton(this, 0);
+    public JoystickButton startButton = new JoystickButton(this, 7);
+    public JoystickButton backButton = new JoystickButton(this, 8);
+    public JoystickButton aButton = new JoystickButton(this, 1);
+    public JoystickButton bButton = new JoystickButton(this, 2);
+    public JoystickButton yButton = new JoystickButton(this, 3);
+    public JoystickButton xButton = new JoystickButton(this, 4);
+    public JoystickButton leftBumper = new JoystickButton(this, 5);
+    public JoystickButton rightBumper = new JoystickButton(this, 6);
+    public POVButton upDPad = new POVButton(this, 0);
+    public POVButton downDPadupDPad = new POVButton(this, 0);
+    public POVButton leftDPad = new POVButton(this, 0);
+    public POVButton rightDPad = new POVButton(this, 0);
 
     public NerdyXboxController(int port) {
         super(port);
-
     }
 
 
