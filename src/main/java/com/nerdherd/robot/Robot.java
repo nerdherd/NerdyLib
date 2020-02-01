dec800951785aecdf34d820c76cec5e13913fe9c
Breakaway <breakaway687@gmail.com>
2020-02-01 11:13:25 -0800

    set up for neo testing

--- src/main/java/com/nerdherd/robot/Robot.java
+++ src/main/java/com/nerdherd/robot/Robot.java
@@ -7,40 +7,12 @@
 
 package com.nerdherd.robot;
 
-import java.util.List;
-
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.nerdherd.lib.drivetrain.auto.DriveStraightContinuous;
-import com.nerdherd.lib.drivetrain.auto.TurnTime;
-import com.nerdherd.lib.drivetrain.auto.TurnToAngle;
-import com.nerdherd.lib.drivetrain.experimental.Drivetrain;
-import com.nerdherd.lib.logging.NerdyBadlog;
-import com.nerdherd.lib.logging.SubscribedLoggable;
-import com.nerdherd.lib.misc.AutoChooser;
-import com.nerdherd.lib.motor.motorcontrollers.CANMotorController;
-import com.nerdherd.lib.motor.motorcontrollers.NerdyTalon;
-import com.nerdherd.lib.motor.motorcontrollers.NerdyVictorSPX;
-import com.nerdherd.lib.motor.single.SingleMotorTalonSRX;
-// import com.playingwithfusion.TimeOfFlight;
-// import com.playingwithfusion.TimeOfFlight.RangingMode;
+import com.nerdherd.lib.motor.motorcontrollers.NerdySparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.controller.RamseteController;
-import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -51,40 +23,21 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
  */
 public class Robot extends TimedRobot {
 
-  public static AutoChooser chooser;
-  public static SubscribedLoggable tester;
-  // public static SingleMotorTalonSRX yeeterTalon;
-  public static Drivetrain m_drive;
-  public static Command m_autonomousCommand;
-  public static SingleMotorTalonSRX m_leftMotor;
-  public static NerdyTalon m_rightMotor;
   public static OI oi;
 
+  public static NerdySparkMax spark1, spark2;
+  
   public Robot() {
     super(0.02);
+    
   }
 
   @Override
   public void robotInit() {
-    // chooser = new AutoChooser();
-    // yeeterTalon = new SingleMotorTalonSRX(5, "flywheel", true, true);
-    // yeeterTalon.configPIDF(0.1, 0, 0, (1023 / 17500));
-    // NerdyBadlog.initAndLog("/media/sda1/logs/", "wooo_testing", 0.02, yeeterTalon);
-    m_drive = new Drivetrain(new NerdyTalon(1), new NerdyTalon(2),  
-        new CANMotorController[] { new NerdyVictorSPX(19), new NerdyVictorSPX(20) },
-        new CANMotorController[] { new NerdyVictorSPX(3), new NerdyVictorSPX(4) }, true, false, 0.63742712872013762571);
-    
-    
-    m_drive.resetEncoders();
-    m_drive.resetYaw();
-        
-    m_drive.configTicksPerFoot(25292.8, 25292.8);
-    m_drive.configSensorPhase(false, false);
-    m_drive.configAutoChooser(chooser);
-    m_drive.configKinematics(0.63742712872013762571, new Rotation2d(0), new Pose2d(0,0, new Rotation2d(0)));
+    spark1 = new NerdySparkMax(1, MotorType.kBrushless);
+    spark2 = new NerdySparkMax(2, MotorType.kBrushless);
+    spark2.follow(spark1);
     oi = new OI();
-    NerdyBadlog.initAndLog("/media/sda1/logs/", "ramseteTuning", 0.02, m_drive);
-    
   }
 
   /**
@@ -98,18 +51,11 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    m_drive.reportToSmartDashboard();
-    // TimeOfFlight m_tof = new TimeOfFlight(0);
-    // m_tof.setRangingMode(RangingMode.Long, 24);
-    // SmartDashboard.putNumber("Distance", m_tof.getRange());
-    // SmartDashboard.putString("Range Mode", m_tof.getRangingMode().toString());
-    // SmartDashboard.putData("Set Short Range", new InstantCommand(() -> m_tof.setRangingMode(RangingMode.Short, 24)));
-    // SmartDashboard.putData("Set Medium Range", new InstantCommand(() -> m_tof.setRangingMode(RangingMode.Medium, 24)));
-    // SmartDashboard.putData("Set Long Range", new InstantCommand(() -> m_tof.setRangingMode(RangingMode.Long, 24)));
-    
-    // SmartDashboard.putData("Set Short Range", new InstantCommand(m_tof.setRangingMode(RangingMode.Short, 24), m_tof));
-    
-    // yeeterTalon.reportToSmartDashboard();
+    SmartDashboard.putNumber("Current 1", spark1.getCurrent());
+    SmartDashboard.putNumber("Current 2", spark2.getCurrent());
+    SmartDashboard.putNumber("Voltage 1", spark1.getVoltage());
+    SmartDashboard.putNumber("Voltage 2", spark2.getVoltage());
+
   }
 
   /**
@@ -140,24 +86,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    
-  //    TrajectoryConfig m_config = new TrajectoryConfig(3, 3);
-  //    Trajectory m_traj = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)) , 
-  //   List.of(
-  //     new Translation2d(0, 2.5)
-  // ), new Pose2d(0, 5, new Rotation2d(0)),
-  //       m_config);
-  //    RamseteCommand ramsete = new RamseteCommand(m_traj, m_drive::getPose2d, new RamseteController(3.0, 0.7), 
-  //                                   new SimpleMotorFeedforward(1.2, 0.241, 0.065), 
-  //                                   m_drive.m_kinematics, m_drive::getCurrentSpeeds, 
-  //                                   new PIDController(3.1, 0, 0), new PIDController(3.1, 0, 0),
-  //                                    m_drive::setVoltage, m_drive);
-    m_autonomousCommand =  new DriveStraightContinuous(m_drive, 10000, 0.8);
-    // m_autonomousCommand =  ramsete.andThen(() -> m_drive.setVoltage(0, 0));
-    // m_autonomousCommand =  null;
-    if (m_autonomousCommand != null) { 
-      m_autonomousCommand.schedule();
-    }
   }
 
   /**
@@ -173,7 +101,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    m_drive.setPose(new Pose2d(3.048, -2.404, new Rotation2d(Math.PI)));
 
   }
 
@@ -183,6 +110,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 CommandScheduler.getInstance().run();
+    spark1.setPower(-1);
   }
 
   /**
