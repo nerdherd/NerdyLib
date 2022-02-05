1a8239dfaa01a7142dad4a53f6da64039a6f0637
easternspork <lazoorcraft@gmail.com>
2022-02-04 23:10:53 -0800

    Add missing import

--- src/main/java/com/nerdherd/lib/pneumatics/AirCompressor.java
+++ src/main/java/com/nerdherd/lib/pneumatics/AirCompressor.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.CompressorConfigType;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AirCompressor extends SubsystemBase {
 
